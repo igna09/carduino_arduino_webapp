d4bca818f6143c34e50d2cd99150484e1d35f127
ignazio abbate <ignazio.abbate95@gmail.com>
2024-01-18 19:26:24 +0100

    improved firmware update

--- src/app/components/firmware-update/firmware-update.component.ts
+++ src/app/components/firmware-update/firmware-update.component.ts
@@ -2,17 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { DragDirective } from '../../dragDrop.directive';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-firmware-update',
   standalone: true,
-  imports: [CommonModule, MatCardModule, DragDirective],
+  imports: [CommonModule, MatCardModule, DragDirective, MatProgressBarModule],
   templateUrl: './firmware-update.component.html',
   styleUrl: './firmware-update.component.scss'
 })
 export class FirmwareUpdateComponent {
   selectedFile: any = undefined;
   @ViewChild('updateForm') updateForm: any;
+  percentage: number = 0;
 
   fileBrowseHandler(event: any) {
     this.selectedFile = event.target.files[0];
@@ -28,16 +30,19 @@ export class FirmwareUpdateComponent {
     let data = new FormData();
     data.append("file", this.selectedFile);
     let request = new XMLHttpRequest();
-    request.open('POST', '/update');
+    request.open('POST', '/update-firmware');
     request.upload.addEventListener('progress', p=>{
-      let w = Math.round((p.loaded / p.total)*100) + '%';
-      if(p.lengthComputable){
-          // prg.innerHTML = w;
-          // prg.style.width = w;
-          console.log(w);
-      }
+      let w = Math.round((p.loaded / p.total)*100);
+      this.percentage = w;
     });
+    request.upload.addEventListener('error', () => this.resetSelectedFile());
+    request.upload.addEventListener('loadend', () => this.resetSelectedFile());
     request.send(data);
+  }
+
+  resetSelectedFile() {
+    this.selectedFile = undefined;
+    this.percentage = 0;
   }
 
 }
