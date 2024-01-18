6f82fafdb028ca33633d3b7d91331172af845f2c
ignazio abbate <ignazio.abbate95@gmail.com>
2024-01-18 17:25:43 +0100

    improved style wip file upload

--- src/app/components/firmware-update/firmware-update.component.ts
+++ src/app/components/firmware-update/firmware-update.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { DragDirective } from '../../dragDrop.directive';
 
 @Component({
   selector: 'app-firmware-update',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, DragDirective],
   templateUrl: './firmware-update.component.html',
   styleUrl: './firmware-update.component.scss'
 })
 export class FirmwareUpdateComponent {
+  selectedFile: any = undefined;
+  @ViewChild('updateForm') updateForm: any;
+
+  fileBrowseHandler(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  fileDropHandler(event: any) {
+    this.selectedFile = event[0].file;
+  }
+
+  sendUpdateFile(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    let data = new FormData(this.updateForm.nativeElement);
+    let request = new XMLHttpRequest();
+    request.open('POST', '/update');
+    request.upload.addEventListener('progress', p=>{
+      let w = Math.round((p.loaded / p.total)*100) + '%';
+      if(p.lengthComputable){
+          // prg.innerHTML = w;
+          // prg.style.width = w;
+          console.log(w);
+      }
+    });
+    request.send(data);
+  }
 
 }
