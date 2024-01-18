fb32fead4c68cf8c0d5b52214bed029e0143807a
ignazio abbate <ignazio.abbate95@gmail.com>
2024-01-18 13:56:07 +0100

    improved test server

--- src/server_test/index.js
+++ src/server_test/index.js
@@ -1,24 +1,28 @@
 const app = require('express')();
 const httpServer = require('http').createServer(app);
-const io = require('socket.io')(httpServer, {
-  cors: {origin : '*'}
-});
+const ws = require('ws');
 
-const port = 3000;
-
-io.on('connection', (socket) => {
+const port = 80;
+let wss = new ws.Server({server: httpServer, path: '/ws'});
+wss.on('connection', (socket) => {
   console.log('a user connected');
 
   socket.on('message', (message) => {
     console.log(message);
-    io.emit('message', `${socket.id.substr(0, 2)}: ${message}`);
+    wss.send('message', `${socket.id.substr(0, 2)}: ${message}`);
   });
 
-  socket.on('disconnect', () => {
+  socket.on('close', () => {
     console.log('a user disconnected!');
   });
 });
 
-setInterval(() => io.emit('message', 'test'),1000) //logs hi every second
+setInterval(() => {
+  wss.clients.forEach(function each(client) {
+    if (client !== ws && client.readyState === ws.OPEN) {
+      client.send("test", { binary: false });
+    }
+  });
+},1000) //logs hi every second
 
 httpServer.listen(port, () => console.log(`listening on port ${port}`));
